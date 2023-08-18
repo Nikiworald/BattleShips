3912c3f47ffff7bba47d688b6007cabdccdaec11
Nikiworald <nikolai.radi@abv.bg>
2023-08-18 15:49:16 +0300

    sql update

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -5,16 +5,10 @@ import map.GameSessionMap;
 import models.GameSession;
 import models.Player;
 import models.PlayerRequest;
-import sql.SqlStatements;
-
-import java.sql.SQLException;
 
 public class Main {
 
     public static void main(String[] args) {
-        GameSession gameSesion = new GameSession();
-        Player player1 = new Player();
-        Player player2 = new Player();
         DbConnection.getConnection();
         PlayerRequest plr1req= new PlayerRequest();
         plr1req.setId(1);
@@ -23,7 +17,7 @@ public class Main {
         plr2req.setId(2);
         plr2req.setName("test2");
         System.out.println( GameSessionMap.createGame(plr1req));//todo no output if successful
-        System.out.println( GameSessionMap.joinGame(6,plr2req));
+        System.out.println( GameSessionMap.joinGame(10,plr2req));
 
 
 
--- src/main/java/sql/SqlStatements.java
+++ src/main/java/sql/SqlStatements.java
@@ -17,19 +17,23 @@ public class SqlStatements {
         ResultSet resultSet = pr.getResultSet();
         return resultSet.next();
     }
-    public static boolean createGame(int player1Id) throws SQLException {
+    public static int createGame(int player1Id) throws SQLException {//todo return int
         Connection dbCon = db.DbConnection.getConnection();
         PreparedStatement pr = null;
         pr= dbCon.prepareStatement("INSERT into gamesessions(status,player1_id) values(?,?)");
         pr.setString(1,"waiting");
         pr.setInt(2,player1Id);
+        pr.executeUpdate();
 //        ResultSet resultSet = pr.getResultSet();
-        return pr.execute();
+        if(pr.getResultSet().next()){
+            return pr.getResultSet().getInt(1);
+        }
+        return -1;
     }
     public static boolean joinGame(int gameId,int player1Id) throws SQLException {
         Connection dbCon = db.DbConnection.getConnection();
         PreparedStatement pr = null;
-        pr= dbCon.prepareStatement("Update gamesessions set player2_id=? where id=?;");
+        pr= dbCon.prepareStatement("Update gamesessions set player2_id=?,status='setup' where id=?;");
         pr.setInt(1,player1Id);
         pr.setInt(2,gameId);
 //        ResultSet resultSet = pr.getResultSet();
